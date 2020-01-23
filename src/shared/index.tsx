6b4fd32ed878b8306cee47745254cd342d6f2e78
pjanik <janikpiotrek@gmail.com>
2020-01-23 22:39:23 +0100

    Update demo page to display experiment schema
    [#170820745]
    [#170820868]
    [#170877065]

--- src/shared/index.tsx
+++ src/shared/index.tsx
@@ -5,74 +5,81 @@ import { Experiment } from "./components/experiment";
 import "./index.sass";
 import { IDataSchema } from "./experiment-schema-types";
 
-ReactDOM.render(
-  <Experiment
-    experimentSchema={{
-      sections: [
-        {
-          title: "Label",
-          formFields: ["studySite", "label", "groupMembers"]
-        },
-        {
-          title: "Measure",
-          formFields: ["experimentData"]
-        }
-      ],
-      dataSchema: {
+const experiment1 = {
+  sections: [
+    {
+      title: "Label",
+      formFields: ["studySite", "label", "groupMembers"]
+    },
+    {
+      title: "Measure",
+      formFields: ["experimentData"]
+    }
+  ],
+  dataSchema: {
+    type: "object",
+    required: ["studySite", "label"],
+    properties: {
+      studySite: {
+        title: "Study Site",
+        type: "string",
+        default: "site1",
+        enum: [
+          "site1",
+          "site2"
+        ],
+        enumNames: [
+          "Site @1",
+          "Site @2"
+        ]
+      },
+      label: {
+        title: "Label",
+        type: "string",
+      },
+      groupMembers: {
+        title: "Group Members",
+        type: "string"
+      },
+      experimentData: {
+        title: "Experiment Data",
         type: "object",
-        required: ["studySite", "label"],
         properties: {
-          studySite: {
-            title: "Study Site",
-            type: "string",
-            default: "site1",
-            enum: [
-              "site1",
-              "site2"
-            ],
-            enumNames: [
-              "Site @1",
-              "Site @2"
-            ]
+          temperature: {
+            title: "Temperature",
+            type: "array",
+            items: {
+              type: ["null", "number"]
+            }
           },
-          label: {
-            title: "Label",
-            type: "string",
+          light: {
+            title: "Light",
+            type: "array",
+            items: {
+              type: ["null", "number"]
+            }
           },
-          groupMembers: {
-            title: "Group Members",
-            type: "string"
-          },
-          experimentData: {
-            title: "Experiment Data",
-            type: "object",
-            properties: {
-              temperature: {
-                title: "Temperature",
-                type: "array",
-                items: {
-                  type: ["null", "number"]
-                }
-              },
-              light: {
-                title: "Light",
-                type: "array",
-                items: {
-                  type: ["null", "number"]
-                }
-              },
-              humidity: {
-                title: "Humidity",
-                type: "array",
-                items: {
-                  type: ["null", "number"]
-                }
-              }
+          humidity: {
+            title: "Humidity",
+            type: "array",
+            items: {
+              type: ["null", "number"]
             }
           }
         }
-      } as IDataSchema
-    }}
-  />,
+      }
+    }
+  } as IDataSchema
+};
+ReactDOM.render(
+  <>
+    <Experiment
+      experimentSchema={experiment1}
+    />
+    <h4>Experiment Schema JSON</h4>
+    <pre>
+      { JSON.stringify(experiment1, null, 2)}
+    </pre>
+  </>,
   document.getElementById("experiment-1")
 );
