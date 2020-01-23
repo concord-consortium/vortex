5d28af730551b2b0015a5581ae53cfc05d2fe3f2
Doug Martin <dmartin@concord.org>
2020-01-23 07:52:56 -0500

    Converted apps to use functional components and removed mobx

--- src/mobile-app/components/app.tsx
+++ src/mobile-app/components/app.tsx
@@ -1,23 +1,12 @@
-import { inject, observer } from "mobx-react";
 import * as React from "react";
-import { BaseComponent, IBaseProps } from "./base";
-import { Text } from "./text";
+import { Text } from "../../shared/components/text";
 
 import "./app.sass";
 
-interface IProps extends IBaseProps {}
-interface IState {}
-
-@inject("stores")
-@observer
-export class AppComponent extends BaseComponent<IProps, IState> {
-
-  public render() {
-    const {ui} = this.stores;
-    return (
-      <div className="app">
-        <Text text={ui.sampleText} />
-      </div>
-    );
-  }
-}
+export const AppComponent = () => {
+  return (
+    <div className="app">
+      <Text text="This is the mobile app" />
+    </div>
+  );
+};
---
+++ src/shared/components/text.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+interface IProps {
+  text: string;
+}
+
+export const Text = (props: IProps) => {
+  const { text } = props;
+  return (
+    <div>
+      {text}
+    </div>
+  );
+};
