1e31832baca49fb134525d3338cb80bd5b435475
pjanik <janikpiotrek@gmail.com>
2020-01-31 00:04:30 +0100

    Add new method of testing hooks, test useRuns hook
    [#170896583]

--- src/shared/test/test-hook.tsx
+++ src/shared/test/test-hook.tsx
@@ -1,6 +1,6 @@
 // adapted from https://dev.to/itsjoekent/write-functional-tests-for-react-hooks-4b07
 
-import React from "react";
+import React, { useState } from "react";
 import { shallow, mount } from "enzyme";
 import { act } from "react-dom/test-utils";
 
@@ -24,4 +24,38 @@ export const testAsyncHook = async (runHook: any, flushEffects = true) => {
     wrapper = flushEffects ? mount(<HookWrapper />) : shallow(<HookWrapper />);
   });
   return wrapper.find("span").prop("data-output");
+};
+
+// Similar to testHook, but it also returns wrapper which lets you continue tests with persisted state. E.g.:
+// const useTick = () => {
+//   const [count, setCount] = useState<number>(0);
+//   const tick = () => setCount(count + 1);
+//   return { count, tick };
+// };
+// describe("use-tick hook", () => {
+//   it("increases count value after calling `tick()`", () => {
+//     const wrapper = testHook2(() => useTick());
+//     let results = wrapper.executeHook();
+//     act(() => {
+//       results.tick();
+//     });
+//     results = wrapper.executeHook();
+//     act(() => {
+//       results.tick();
+//     });
+//     results = wrapper.executeHook();
+//     expect(results.count).toEqual(2);
+//   });
+// });
+export const getHookWrapper = (runHook: any, flushEffects = true) => {
+  const HookWrapper = () => <span data-output={runHook()} />;
+  let wrapper: any;
+  act(() => {
+    wrapper = flushEffects ? mount(<HookWrapper />) : shallow(<HookWrapper />);
+  });
+  wrapper.executeHook = () => {
+    wrapper.update();
+    return wrapper.find("span").prop("data-output");
+  };
+  return wrapper;
 };
