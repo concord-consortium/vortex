5efcb414be712a584c95e42185685a3ddb4b1c1b
Doug Martin <dmartin@concord.org>
2020-02-07 08:48:31 -0500

    Added serialize-promises.test.ts

---
+++ src/shared/utils/serialize-promises.test.ts
@@ -0,0 +1,19 @@
+import { serializePromises } from "./serialize-promises";
+
+describe("serializePromises", () => {
+  it("acts the same as Promise.all", async () => {
+    const promiseAll = await Promise.all([Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)]);
+    const serialize = await serializePromises([Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)]);
+    expect(serialize).toEqual([1, 2, 3]);
+    expect(promiseAll).toEqual(serialize);
+  });
+
+  it("allows for timeouts between promises", async () => {
+    const delay = 10;
+    const before = Date.now();
+    const serialize = await serializePromises([Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)], delay);
+    const after = Date.now();
+    expect(serialize).toEqual([1, 2, 3]);
+    expect(after - before).toBeGreaterThanOrEqual(delay * serialize.length);
+  });
+});
