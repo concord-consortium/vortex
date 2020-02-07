122a65ddf01f6ae4733e03b50e9af86a55c2b4d9
Doug Martin <dmartin@concord.org>
2020-02-07 08:41:25 -0500

    Added delay-promise.test.ts

---
+++ src/shared/utils/delay-promise.test.ts
@@ -0,0 +1,12 @@
+import { delayPromise } from "./delay-promise";
+
+describe("delayPromise", () => {
+  it("returns a promise that resolves to the value after a delay", async () => {
+    const delay = 20;
+    const before = Date.now();
+    const result = await delayPromise(delay, "foo");
+    const after = Date.now();
+    expect(result).toBe("foo");
+    expect(after - before).toBeGreaterThanOrEqual(delay);
+  })
+});
