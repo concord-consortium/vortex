e3c04c5fafa407e45aafd63d9c5ed49018e00747
Kevin Santos <ksantos@CC-KSANTOS.local>
2020-02-10 17:44:56 -0500

    fix URL used in tests

--- cypress/integration/workspace.test.ts
+++ cypress/integration/workspace.test.ts
@@ -1,11 +1,11 @@
 context("Test the overall app", () => {
   beforeEach(() => {
-    cy.visit("");
+    cy.visit("/mobile-app/");
   });
 
   describe("Desktop functionalities", () => {
     it("renders with text", () => {
-      cy.get(".app").should("have.text", "Hello World");
+      cy.get("#app");
     });
   });
 });
