75146ee8048b1de95a27edbca23a8e7d2e8aa80c
pjanik <janikpiotrek@gmail.com>
2020-02-10 12:36:52 +0100

    Fix format-time test

--- src/shared/utils/format-time.test.ts
+++ src/shared/utils/format-time.test.ts
@@ -2,7 +2,7 @@ import { formatTime } from "./format-time";
 
 describe("format-time", () => {
   it("formats correctly", () => {
-    // replace hour with X to remove timezone different from Travis servers
-    expect(formatTime(1581082141929).replace("8:", "X:")).toBe("Fri, February 7, 2020, X:29 AM");
+    // use regex to handle various time zones
+    expect(formatTime(1581082141929)).toMatch(/Fri, February 7, 2020, \d?\d:29 [AP]M/);
   });
 });
