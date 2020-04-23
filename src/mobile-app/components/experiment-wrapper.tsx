6ef095295f1f96a85ea6b5a092a174c356746c1e
Chris Hart <chart@concord.org>
2020-04-23 16:29:22 -0400

    Enable experiment rename in the app

--- src/mobile-app/components/experiment-wrapper.tsx
+++ src/mobile-app/components/experiment-wrapper.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { IExperiment, IExperimentConfig, IExperimentData } from "../../shared/experiment-types";
 import { Experiment } from "../../shared/components/experiment";
 import { Initials } from "../../shared/components/initials";
@@ -26,7 +26,8 @@ const experimentConfig: IExperimentConfig = {
   showCameraButton: true
 };
 
-export const ExperimentWrapper: React.FC<IProps> = ({ experiment, experimentIdx, data, onDataChange, onBackBtnClick, onUpload, embeddedPreview}) => {
+export const ExperimentWrapper: React.FC<IProps> = ({ experiment, experimentIdx, data, onDataChange, onBackBtnClick, onUpload, embeddedPreview }) => {
+  const [editing, isEditing ] = useState(false);
   const { metadata } = experiment;
   const { initials } = metadata;
   const workSpaceClass = embeddedPreview ? `${css.workspace} ${css.embeddedPreview}`: css.workspace;
@@ -34,6 +35,26 @@ export const ExperimentWrapper: React.FC<IProps> = ({ experiment, experimentIdx,
   const handleSave = () => {
     // blur the active input element if focused
     (document.activeElement as HTMLElement)?.blur?.();
+  };
+
+  const handleRename = () => {
+    if (editing) {
+      onDataChange(data);
+    }
+    isEditing(!editing);
+  };
+  const handleEnter = (e: any) => {
+    if (e.keyCode === 13) {
+      if (editing) {
+        handleRename();
+      }
+    }
+  };
+  const saveExperimentName = (el: any) => {
+    const newName = el.currentTarget.value;
+    if (newName) {
+      experiment.metadata.name = newName;
+    }
   };
 
   let title = null;
@@ -46,13 +67,22 @@ export const ExperimentWrapper: React.FC<IProps> = ({ experiment, experimentIdx,
         <div className={css.headerBackIcon} onClick={onBackBtnClick}><Icon name="arrow_back"/></div>
         <Initials text={initials} active={true}/>
         <div className={css.headerTitle}>
-          <div>{`${experiment.metadata.name} #${experimentIdx}`}</div>
+          {editing &&
+            <div className={css.editing}>
+              <input defaultValue={`${metadata.name}`} onChange={saveExperimentName} onBlur={handleRename} onKeyDown={handleEnter}/>
+              <div className={css.stopEditing} onClick={handleRename}>&#10003;</div>
+            </div>
+          }
+          {!editing &&
+            <div><span onClick={handleRename}>{`${metadata.name} #${experimentIdx}`}</span></div>
+          }
           <div>{title}</div>
         </div>
         <div className={css.headerMenu}>
           <MenuComponent>
             <MenuItemComponent onClick={handleSave}>Save</MenuItemComponent>
             <MenuItemComponent onClick={onUpload}>Upload</MenuItemComponent>
+            <MenuItemComponent onClick={handleRename}>Rename</MenuItemComponent>
           </MenuComponent>
         </div>
       </div>
