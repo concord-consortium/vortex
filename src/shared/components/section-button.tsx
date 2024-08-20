8bebafbff8da2bba4fc99c4ece834fe6ed210f05
Doug Martin <dmartin@concord.org>
2024-08-20 14:46:30 -0400

    build: Disable camera due to iOS build issues

--- src/shared/components/section-button.tsx
+++ src/shared/components/section-button.tsx
@@ -11,8 +11,15 @@ interface IProps {
   onClick?: (event: React.MouseEvent) => void;
 }
 
-export const SectionButton: React.FC<IProps> = ({ title, icon, active, disabled, onClick }) =>
-  <div className={classNames(css.sectionButton, {[css.disabled]: disabled})} onClick={disabled ? undefined : onClick}>
-    <div className={css.icon + (active ? ` ${css.active}` : "")}><Icon name={icon} /></div>
-    { title }
-  </div>;
+export const SectionButton: React.FC<IProps> = ({ title, icon, active, disabled, onClick }) => {
+  // NOPHOTO: Removed the photo element due to iOS build issues
+  if (title === "Note & Photo") {
+    title = "Notes";
+  }
+  return (
+    <div className={classNames(css.sectionButton, {[css.disabled]: disabled})} onClick={disabled ? undefined : onClick}>
+      <div className={css.icon + (active ? ` ${css.active}` : "")}><Icon name={icon} /></div>
+      { title }
+    </div>
+  );
+};
